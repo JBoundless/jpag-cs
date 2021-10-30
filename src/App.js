9be8372d4009ae8ba0eba150acc4843372c47b58
Jordan Pagkalinawan <39219944+JBoundless@users.noreply.github.com>
2021-10-30 14:12:34 -0700

    Align h2's center

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ export default function App() {
       <h1>Jordan Pagkalinawan's Computer Science Portfolio</h1>
       <Header />
       </section>
-      <h2 style={{paddingTop: '10px', paddingBottom: '10px'}}>Languages I've Learned</h2>
+      <h2 style={{paddingTop: '10px', paddingBottom: '10px', textAlign: 'center'}}>Languages I've Learned</h2>
       <LanguageList />
-      <h2 style={{paddingTop: '10px', paddingBottom: '10px'}}>My Coding Projects</h2>
-      <p style={{paddingTop: '10px', paddingBottom: '10px'}}>For more projects, visit <a href="https://github.com/JBoundless">my GitHub profile</a>.</p>
+      <h2 style={{paddingTop: '10px', paddingBottom: '10px', textAlign: 'center'}}>My Coding Projects</h2>
+      <p style={{paddingTop: '10px', paddingBottom: '10px', textAlign: 'center'}}>For more projects, visit <a href="https://github.com/JBoundless">my GitHub profile</a>.</p>
       <ProjectCards />
     </div>
   );
